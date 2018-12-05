8aca88329de5abef39cb4da15082eb00dcd9aee4
Ali <ali198.98@mail.ru>
2018-12-05 19:32:58 +0400

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+console.log(4/0);
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plug.png', 'orange.jpg', 'apple.bmp'];
+
+//alert("Hello world!");
+let answer = prompt("Есть ли вам 18?", "Да");
+console.log(typeof(answer));
